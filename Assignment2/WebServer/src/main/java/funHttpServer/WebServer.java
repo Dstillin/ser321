049ffbd9a37004803bc26b6c4eb921296311e4ad
GTSimpson <gtsimpso@asu.edu>
2021-10-27 14:22:41 -0700

    "Add error handling for multiply"

--- Assignment2/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Assignment2/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -201,21 +201,41 @@ class WebServer {
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
 
-          // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+          //Check for correct parameters
+          if(query_pairs.containsKey("num1") & query_pairs.containsKey("num2")){
 
-          // do math
-          Integer result = num1 * num2;
+            //Get both arguments
+            String stringNum1 = query_pairs.get("num1");
+            String stringNum2 = query_pairs.get("num2");
 
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
+            //attempt to convert string to int and do math
+            try {
+              int num1 = Integer.parseInt(query_pairs.get("num1"));
+              int num2 = Integer.parseInt(query_pairs.get("num2"));
+              int result = num1 * num2;
+              builder.append("HTTP/1.1 200 OK\n");
+              builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append("Result is: " + result);
+            } catch (NumberFormatException e) {
+              builder.append("HTTP/1.1 400 Bad Request\n");
+              builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append("Arguments num1 and num2 require two integers.\n");
+              builder.append("\n");
+              builder.append("Example: multiply?num1=4&num2=4");
+            }
 
-          // TODO: Include error handling here with a correct error code and
-          // a response that makes sense
+            //Fail condition for incorrect arguments.
+          } else{
+            builder.append("HTTP/1.1 400 Bad Request\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Multiply requires two parameters, num1=[Value] and num2=[Value].\n");
+            builder.append("\n");
+            builder.append("Example: multiply?num1=4&num2=4");
+
+          }
 
         } else if (request.contains("github?")) {
           // pulls the query from the request and runs it with GitHub's REST API
